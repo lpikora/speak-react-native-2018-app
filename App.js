cda93f5570a7a41b9dccfe105473b7e02958fccc
Lukas Pikora <lpikora@gmail.com>
2018-11-15 19:24:44 +0100

    feat(tslint): basic setup tslint, autoimports

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, Title } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ImageComponent from './components/image/image.component';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Image source={require("./assets/icon.png")} />
+        <ImageComponent />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
---
+++ components/image/image.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class ImageComponent extends Component {
+  render() {
+    const {} = this.props;
+
+    return (
+      <View>
+        <Text>helloo!</Text>
+      </View>
+    );
+  }
+}
